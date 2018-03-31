df6e4415f1c4feb7a24ed78db8c5891c204230ea
donrestarone <shashikejayatunge@gmail.com>
2018-03-31 13:02:31 -0400

    so many errors, dont know what to do

--- quote-collector.rb
+++ quote-collector.rb
@@ -1,25 +1,47 @@
-#require_relative "multilinguist.rb"
-quote_list = ["The second child class we're going to define
+require_relative "multilinguist.rb"
+
+
+
+class Quote_collector < Multilinguist
+
+@@quote_list = ["The second child class we're going to define
  represents a person who loves to memorize quotes and then travel the world, 
  unleashing poor translations of them to unsuspecting passers-by", "Each instance of this class should have 
  its own ever-growing collection of favourite quotes.", "It should have the ability to add a new quote to its 
  collection as well as the ability to select a random quote to share in the local language."]
 
-class Quote_collector #< Multilinguist
-	def add_quote_to_list(quote, destination_list)
-		destination_list.push(quote)
+	def add_quote_to_list(quote)
+		@@quote_list.push(quote)
 	end 
-	def read_quotes_at_random(quote_list)
-	random_quote =quote_list.sample
-	return random_quote 
 
+	def read_quotes_at_random
+	random_quote = @@quote_list.sample
+	return random_quote 
 	end 
+
+	def all_quotes 
+		return @@quote_list
+	end
 end 
+
+#testing
 
 collector1 = Quote_collector.new 
 
-collector1.add_quote_to_list("the candle that burns twice as bright lasts half as long", quote_list)
+collector1.add_quote_to_list("the candle that burns twice as bright lasts half as long")
 
-puts quote_list.inspect
+puts collector1.all_quotes.inspect
 
-puts collector1.read_quotes_at_random(quote_list).inspect
+puts collector1.read_quotes_at_random.inspect
+
+puts 
+puts
+puts collector1.all_quotes.inspect
+
+#inherit from Multilinguist class
+collector1.travel_to("india")
+
+collector1.say_in_local_language(read_quotes_at_random)
+
+
+
